1c0d1dce186eb97015767108d6052a36e9788d05
Anatoly <tberchanov@gmail.com>
2017-05-13 17:11:46 +0300

    Закоментил неиспользуемые поля в SendMessageModel

--- app/models/telegramModels/SendMessageModel.java
+++ app/models/telegramModels/SendMessageModel.java
@@ -9,10 +9,11 @@ import java.util.List;
 public class SendMessageModel {
     public int chat_id;
     public String text;
-    public String parse_mode;
-    public boolean disable_web_page_preview;
-    public boolean disable_notification;
-    public int reply_to_message_id;
+//    когда есть неиспользуемые поля, то не работает, по этому пока закоментил.
+//    public String parse_mode;
+//    public boolean disable_web_page_preview;
+//    public boolean disable_notification;
+//    public int reply_to_message_id;
     public Keyboard reply_markup;
 
     public SendMessageModel() {
